035f23f19975896fbab78167eb7574927a1057e9
Trung Le <leqtrungit@gmail.com>
2024-05-04 21:09:44 +0700

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    host: true,
+    // port: 5173,
+    // hmr: {
+    //   host: "0.0.0.0",
+    //   clientPort: 5173
+    // }
+    watch: {
+      usePolling: true,
+      interval: 500
+    }
+  }
+})
